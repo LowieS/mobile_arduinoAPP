52f49c02f26b6de2591dcbc9657efd8753d55974
lowie spriet <lowie.spriet@gmail.com>
2018-12-05 17:04:45 +0100

    images + first mockup inplement
    main button een image button van gemaakt deze veranderd als je er op kilkt ook is deze button gelinkt aan de connection methode

--- app/src/main/java/com/example/lsp/cardom/MainActivity.java
+++ app/src/main/java/com/example/lsp/cardom/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView) findViewById(R.id.textView);
+        //textView = (TextView) findViewById(R.id.textView);
 
 
     }

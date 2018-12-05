a401e437f67116354a7c09ba10451e06b4a8745d
lowie spriet <lowie.spriet@gmail.com>
2018-12-05 12:32:39 +0100

    arduino test met lampen

--- arduinoV1/arduinoV1.ino
+++ arduinoV1/arduinoV1.ino
@@ -1,7 +1,9 @@
 char c;
 void setup()  
 {  
-  Serial.begin(9600);            // Start hardware Serial  
+  Serial.begin(9600);   
+  pinMode(2,OUTPUT); 
+  pinMode(3,OUTPUT);  
  
 }  
   
@@ -16,11 +18,16 @@ void loop()
        if(c=='t'){
         
         DataT();
+
+        digitalWrite(2,HIGH);
+        digitalWrite(3,LOW);
        }
 
        if(c=='f'){
         
         DataF();
+        digitalWrite(3,HIGH);
+        digitalWrite(2,LOW);
        }
         
      

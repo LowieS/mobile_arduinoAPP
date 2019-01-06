fa24bc15d7acae59edb2cf6777e90140814b79de
MichielVissers <michiel.vissers09@gmail.com>
2019-01-06 16:07:03 +0100

    lights enable and disable

--- app/src/main/java/com/example/lsp/cardom/Lights.java
+++ app/src/main/java/com/example/lsp/cardom/Lights.java
@@ -3,14 +3,19 @@ package com.example.lsp.cardom;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Lights extends AppCompatActivity {
     boolean mBounded;
+    boolean GROOTLICHT = false;
+    boolean DIMLICHT = false;
     BlueServer mServer;
 
     @Override
@@ -54,8 +59,16 @@ public class Lights extends AppCompatActivity {
     };
 
     public void SEND1(View view) {
-
-
+        GROOTLICHT = !GROOTLICHT;
+        Button GROOTLICHTbut = findViewById(R.id.button8);
+        if(GROOTLICHT == true){
+            GROOTLICHTbut.setBackgroundResource(R.drawable.lights_enabled);
+            GROOTLICHTbut.setTextColor(Color.parseColor("#0000FF"));
+        }
+        else if(GROOTLICHT == false){
+            GROOTLICHTbut.setBackgroundResource(R.drawable.lights_disabled);
+            GROOTLICHTbut.setTextColor(Color.parseColor("#CBCBCB"));
+        }
 
         mServer.MyBlue.Send("1");
 
@@ -63,7 +76,16 @@ public class Lights extends AppCompatActivity {
     }
 
     public void SEND2(View view) {
-
+        DIMLICHT = !DIMLICHT;
+        Button DIMLICHTbut = findViewById(R.id.button8);
+        if(DIMLICHT == true){
+            DIMLICHTbut.setBackgroundResource(R.drawable.lights_enabled);
+            DIMLICHTbut.setTextColor(Color.parseColor("#0000FF"));
+        }
+        else if(GROOTLICHT == false){
+            DIMLICHTbut.setBackgroundResource(R.drawable.lights_disabled);
+            DIMLICHTbut.setTextColor(Color.parseColor("#CBCBCB"));
+        }
 
         mServer.MyBlue.Send("2");
     }
